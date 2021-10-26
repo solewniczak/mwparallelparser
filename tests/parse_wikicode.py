7779c6b384e48546027b4d10d5c1ac7f674a7aa1
Szymon Olewniczak <szyolewn@pg.edu.pl>
2021-10-26 10:27:18 +0200

    add parse_wikicode to tests

---
+++ tests/parse_wikicode.py
@@ -0,0 +1,11 @@
+import os
+
+from src.mwparallelparser import Parser
+
+filepath = os.path.join(os.path.dirname(__file__), 'wikicode')
+with open(filepath) as file:
+    wikitext = file.read()
+parser = Parser()
+result = parser.parse(wikitext)
+print(result['lines'])
+print(result['links'])
